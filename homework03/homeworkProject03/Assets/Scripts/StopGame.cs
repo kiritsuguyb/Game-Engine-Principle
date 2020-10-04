433807f5cb2f411674db6bbecc794f119f20c63e
kiritsuguyb <1125010742@qq.com>
2020-10-04 17:29:53 +0800

    car game

---
+++ homework03/homeworkProject03/Assets/Scripts/StopGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StopGame : MonoBehaviour
+{
+    public bool win = false;
+    public bool lost = false;
+    public Text gameoverText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameoverText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)) Application.Quit();
+        if (!win && !lost) return;
+        Time.timeScale = 0;
+        if (lost) gameoverText.text = "Score:" + 0;
+        else gameoverText.text = "Score:" + FindObjectOfType<Player>().score;
+        gameoverText.gameObject.SetActive(true);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody.gameObject.tag.Equals("Player"))
+        {
+            win = true;
+        }
+    }
+}
